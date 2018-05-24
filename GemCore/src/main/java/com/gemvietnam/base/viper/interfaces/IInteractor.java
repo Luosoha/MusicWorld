55c20a76282975b958787360217c5ffb9a301979
Hai Luong <hails@gemvietnam.com>
2018-05-24 10:09:01 +0700

    removed gem core

--- GemCore/src/main/java/com/gemvietnam/base/viper/interfaces/IInteractor.java
+++
@@ -1,9 +0,0 @@
-package com.gemvietnam.base.viper.interfaces;
-
-/**
- * Base Interactor
- * Created by neo on 8/29/2016.
- */
-public interface IInteractor<P extends IPresenter> {
-  P getPresenter();
-}
